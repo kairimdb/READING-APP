a68fcdb9403393b400dec8c513025d5905ab51e4
Kairi Bautista <kairimdb@gmail.com>
2024-02-28 22:12:54 +0800

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace READING_APP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
